81e8544e1242fbb5509d7ebcdd0f553b4cd0620f
wesmags <wesmags22@outlook.com>
2022-07-21 13:24:14 +1000

    Added profiler.py
    Created a simple profiler main file.

---
+++ profiler.py
@@ -0,0 +1,16 @@
+# Debug Program
+
+# A simpler, backwards compatible function
+# Works with old python versions
+class debug_legacy:
+    def __init__(self):
+        pass
+    def e_log(self):
+        pass
+    
+# Main debugger 
+class debug:
+    def __init__(self):
+        pass
+    def e_log(self):
+        pass
